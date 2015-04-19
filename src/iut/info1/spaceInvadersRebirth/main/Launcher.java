682c15172b3f7fd3218cf5367d8f111dad342345
MrBin99 <paul.pages12@gmail.com>
2015-04-19 21:00:56 +0200

    Adding Eclipse Project, Visual Paradigm Project.

---
+++ src/iut/info1/spaceInvadersRebirth/main/Launcher.java
@@ -0,0 +1,20 @@
+/*
+ * Launcher.java
+ */
+package iut.info1.spaceInvadersRebirth.main;
+
+/**
+ * Classe contenant la méthode "Main" permettant de lancer l'application.
+ * @author MrBin99
+ * @version dev
+ */
+public class Launcher {
+
+    /**
+     * Lance le jeu.
+     * @param args non utilisés.
+     */
+    public static void main(String[] args) {
+        
+    }
+}

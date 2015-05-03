d25aaca5740d0a42a63657e93a4b0f34ddbcf493
MrBin99 <paul.pages12@gmail.com>
2015-05-03 15:34:00 +0200

    Adding test folder.

---
+++ test/iut/info1/spaceInvadersRebirth/main/TestLauncher.java
@@ -0,0 +1,20 @@
+/*
+ * Launcher.java
+ */
+package iut.info1.spaceInvadersRebirth.main;
+
+/**
+ * Classe de test pour la classe {@link Launcher}.
+ * @author MrBin99
+ * @version dev
+ */
+public class TestLauncher {
+
+    /**
+     * Lance les tests.
+     * @param args non utilisés.
+     */
+    public static void main(String[] args) {
+        
+    }
+}

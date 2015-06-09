f33bca89758f919b098ab9134771eafe3b38a134
MrBin99 <paul.pages12@gmail.com>
2015-06-09 13:05:53 +0200

    Resolved bug of concurrent access on a variable between graphical thread and main thread.

--- src/iut/info1/spaceInvadersRebirth/gui/GamePanel.java
+++ src/iut/info1/spaceInvadersRebirth/gui/GamePanel.java
@@ -4,15 +4,17 @@
 package iut.info1.spaceInvadersRebirth.gui;
 
 import iut.info1.spaceInvadersRebirth.gameStates.GameStateManager;
+
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 /**
  * Représente le panneau de jeu où sera dessiné tous les éléments du jeu.
@@ -71,17 +73,16 @@ public class GamePanel extends JPanel implements KeyListener {
     /** Créé le Timer permettant de lancer le jeu. */
     public void run() {
         // Créé l'horloge qui "tick" toute les 16ms environ
-        ticker = new Timer();
-        ticker.schedule(new TimerTask() {
+        ticker = new Timer(1000 / FPS, new ActionListener() {
             
-            // A chaque tick du jeu
             @Override
-            public void run() {
+            public void actionPerformed(ActionEvent e) {
                 gameStateManager.update();
                 repaint();
             }
-            
-        }, 0, 1000 / FPS);
+        });
+        
+        ticker.start();
     }
     
     /* 
